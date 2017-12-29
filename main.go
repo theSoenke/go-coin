cc9f20d4bf7d5833aa330fab663df63aca0f9612
theSoenke <theSoenke@users.noreply.github.com>
2017-12-29 12:08:56 +0100

    Init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+const targetBits = 24
+
+func main() {
+	bc := NewBlockchain()
+	bc.AddBlock("Send 1 Coin to Bob")
+	bc.AddBlock("Send 3 Coin to Anna")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+}
+
+// IntToHex converts an int64 to a byte array
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
